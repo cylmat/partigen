3b632a12cb84febe280dc043747fe5661aa15aeb
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-06-22 20:54:34 +0200

    Update PartitionBlock.php

--- src/Model/Block/PartitionBlock.php
+++ src/Model/Block/PartitionBlock.php
@@ -7,25 +7,30 @@ namespace Partigen\Model\Block;
 use Partigen\DataValue\ScopeDataValueFactory;
 use Partigen\Model\BlockFactory;
 use Partigen\Model\Params;
+use Partigen\Service\Randomizer;
 
 class PartitionBlock extends AbstractBlock
 {
     private const SCOPES_NUMBER_IN_PAGE = 6;
 
     private ScopeDataValueFactory $dataValueFactory;
+    private Randomizer $randomizer;
 
-    public function __construct(BlockFactory $factory, ScopeDataValueFactory $dataValueFactory)
-    {
+    public function __construct(
+        BlockFactory $factory,
+        ScopeDataValueFactory $dataValueFactory,
+        Randomizer $randomizer
+    ) {
         parent::__construct($factory);
         $this->dataValueFactory = $dataValueFactory;
+        $this->randomizer = $randomizer;
     }
 
     public function getData(Params $context): array
     {
         $partitionData = [];
-        $contextScopes = \array_flip($context->getScopes());
         for ($s=0; $s<self::SCOPES_NUMBER_IN_PAGE; $s++) {
-            $selectedScope = \array_rand($contextScopes);
+            $selectedScope = $this->randomizer->getScope($context->getScopes());
             $partitionData[] = $this->get(ScopeBlock::class)
                 ->setScopeData($this->dataValueFactory->create($selectedScope))
                 ->getData($context);
